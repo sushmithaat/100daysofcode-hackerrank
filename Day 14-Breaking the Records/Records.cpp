a335d2a20dd1ed2e14024d42846ae00a705722f3
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-04-03 17:52:23 +0530

    Create Records.cpp

---
+++ Day 14-Breaking the Records/Records.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int maxc=0,minc=0;
+    int max,min; 
+
+    cin>>n;
+
+    vector<int> s(n);
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>s[i];
+    }    
+    max=s[0];
+    min=s[0];
+    for(int i=1;i<n;i++)
+    {
+        if(s[i]>max)
+        {
+            max=s[i];
+            maxc++;
+        }
+        else if(s[i]<min)
+        {
+            min=s[i];
+            minc++;
+        }
+    }
+    cout<<maxc<<" "<<minc;
+    return 0;
+}
