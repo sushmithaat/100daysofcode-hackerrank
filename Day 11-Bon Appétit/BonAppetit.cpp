032c98d51a891380f906e92b5d5dcc1d7fefc3c4
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-04-03 17:17:14 +0530

    Create BonAppetit.cpp

---
+++ Day 11-Bon Appétit/BonAppetit.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,k,b,sum,ac=0;
+
+    cin>>n>>k;
+
+    vector<int> bill(n);
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>bill[i];
+    }
+    cin>>b;
+
+    sum=accumulate(bill.begin(),bill.end(),0); //accumulate function is used to find the sum of elements in vector(bill)
+    for(int i=0;i<n;i++)
+    {
+        if(k==i)
+           sum=sum-bill[k]; //subtracting the bill of Anna didn't eat 
+    }
+    ac=sum/2; //finding the actual cost per two person by dividing the sum by 2
+    if(ac==b) //when actual bill amount is equal to charged bill print 'Bon Appetit'
+        cout<<"Bon Appetit\n";
+    else    
+        cout<<b-ac<<endl; //otherwise subtract the charged bill with actual bill 
+    
+    return 0;
+}
