4a212a3b0a834dea63f6e281d51ade46b977dcea
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-04-04 13:21:34 +0530

    Update justFunction.cpp

--- Day 16-Divisible Sum Pairs/justFunction.cpp
+++ Day 16-Divisible Sum Pairs/justFunction.cpp
@@ -1,3 +1,5 @@
+//Problem link-https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
 // Complete the divisibleSumPairs function below.
 int divisibleSumPairs(int n, int k, vector<int> ar) 
 {
@@ -6,9 +8,9 @@ int divisibleSumPairs(int n, int k, vector<int> ar)
     {
         for(int j=i+1;j<n;j++)
         {
-            if((ar[i]+ar[j])%k ==0)
+            if((ar[i]+ar[j])%k ==0) //when sum of ar[i]+a[j] is divisible by k then increment the count value by 1
                 count++;
         }
     }
-    return count;
+    return count; //return the count value
 }
