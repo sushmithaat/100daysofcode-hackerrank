a0847f64249d64c61fe2fefdc67814138e5e4e18
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-04-14 18:26:42 +0530

    Create postorder.cpp

---
+++ Day 21-Tree: Postorder Traversal/postorder.cpp
@@ -0,0 +1,34 @@
+//Problem Link-https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node 
+{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) 
+        {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+    void postOrder(Node *root) 
+    {
+        if(root!=NULL)
+        {
+            postOrder(root->left);
+            postOrder(root->right);
+            cout<<root->data<<" ";
+        }
+
+    }
+
+}; //End of Solution
